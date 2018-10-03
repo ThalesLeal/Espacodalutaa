d3a3e611d697432650416251f5fa3f735b4bc7ed
Thales Leal <thales.b.leall@gmail.com>
2018-10-03 13:46:50 -0300

    subindo novo projeto

---
+++ catalog/views.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+
+from django.shortcuts import render,get_object_or_404
+
+from .models import Product, Category
+
+
+def product_list(request):
+    context = {
+        'product_list': Product.objects.all()
+    }
+    return render(request, 'catalog/product_list.html', context)
+
+
+def category(request, slug):
+    category = Category.objects.get(slug=slug)
+    context = {
+        'current_category': category,
+        'product_list': Product.objects.filter(category=category),
+    }
+    return render(request, 'catalog/category.html', context)
+
+
+def product(request, slug):
+    product = Product.objects.filter(slug=slug)
+    context = {
+        'current_product': product,
+        'product_list': Product.objects.filter(slug=product),
+        }
+    return render(request, 'catalog/product.html', context)
---
+++ core/views.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from catalog.models import Category
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
