905a76651d1045d3cb755671fed67bc4adcfa129
Thales Leal <thales.b.leall@gmail.com>
2018-10-10 09:18:37 -0300

    teste

--- Espacodaluta/wsgi.py
+++ Espacodaluta/wsgi.py
@@ -6,22 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
-# 
-# import os
-#
-# from django.core.wsgi import get_wsgi_application
-#
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Espacodaluta.settings")
-#
-# application = get_wsgi_application()
-
-
-
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kloudless.settings")
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Espacodaluta.settings")
+
+application = get_wsgi_application()
